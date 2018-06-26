62b20d72a8e76865ab180ae020136ea83a94bcb5
Benoit Hamelin <benoit@benoithamelin.com>
2018-06-26 18:24:48 -0400

    Tag image with both Cypht and own versions

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 path_repo=$(dirname $0)
 path_cypht="$path_repo/cypht"
 pushd "$path_cypht"
-cypht_version=$(git describe --always --tags)
+version_cypht=$(git describe --always --tags)
 popd
 cd "$path_repo"
+version_image=$(git describe --always)
 user_docker=$(jq --raw-output '.auths."https://index.docker.io/v1/".auth' $HOME/.docker/config.json \
     | base64 -d \
     | awk -F: '1{print $1}')
 test "$?" -eq 0 || exit $?
-tag_docker="$user_docker/cypht:$cypht_version"
+tag_docker="$user_docker/cypht:$version_cypht--$version_image"
 cmd="docker build --tag $tag_docker ."
 echo "Building: $cmd"
 $cmd || exit $?
-echo "$tag_docker" >&3
